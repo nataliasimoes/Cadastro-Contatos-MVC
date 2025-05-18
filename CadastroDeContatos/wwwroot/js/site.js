8a4eeeda0a26b98548715b4f73ec10bd5d875252
Natália Simões <natalia.simoes.dev@gmail.com>
2025-05-18 20:54:31 -0300

    Add DataTables library

--- CadastroDeContatos/wwwroot/js/site.js
+++ CadastroDeContatos/wwwroot/js/site.js
@@ -2,3 +2,51 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+    // Verifica se a tabela existe na página
+    if ($('#table-contatos').length) {
+        // Inicializa o DataTable apenas uma vez com todas as configurações
+        $('#table-contatos').DataTable({
+            language: {
+                "decimal": ",",
+                "thousands": ".",
+                "processing": "Processando...",
+                "lengthMenu": "_MENU_ Registros por página",
+                "zeroRecords": "Nenhum registro encontrado",
+                "info": "Mostrando _START_ a _END_ de _TOTAL_ registros",
+                "infoEmpty": "Mostrando 0 a 0 de 0 registros",
+                "infoFiltered": "(filtrado de _MAX_ registros no total)",
+                "search": "Pesquisar: ",
+                "paginate": {
+                    "first": "Primeiro",
+                    "last": "Último",
+                    "next": "Próximo",
+                    "previous": "Anterior"
+                },
+                "aria": {
+                    "sortAscending": ": ativar para ordenar coluna em ordem crescente",
+                    "sortDescending": ": ativar para ordenar coluna em ordem decrescente"
+                }
+            },
+            layout: {
+                bottomEnd: {
+                    paging: {
+                        firstLast: false
+                    }
+                }
+            },
+            info: false,
+            columnDefs: [
+                {
+                    target: 3, 
+                    orderable: false 
+                },
+                {
+                    target: 4,
+                    orderable: false
+                }
+            ]
+        });
+    }
+});
